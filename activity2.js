abed8b314c014bd79b90c1332cf68e836be16f2b
vrindaisthebest <88569278+vrindaisthebest@users.noreply.github.com>
2021-08-29 13:02:19 +1000

    Add files via upload

---
+++ activity2.js
@@ -0,0 +1,12 @@
+function back()
+
+{
+    window.location = "activity_1.html";
+}
+
+function getScore() {
+
+    score = localStorage.getItem ("score");
+
+    document.getElementById("update").innerHTML = " <h1> Score: " + score + "</h1>";
+}
